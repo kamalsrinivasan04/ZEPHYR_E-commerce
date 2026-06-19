48198cf39f484e02834d93e7812abc415cb98726
kamalsrinivasan04 <kamalnayans1864@gmail.com>
2026-06-19 16:30:42 +0530

    Product sorting added

--- src/pages/Products/index.jsx
+++ src/pages/Products/index.jsx
@@ -1,7 +1,43 @@
+import { useState } from "react";
 import products from "../../data/products.json";
 import ProductCard from "../../components/ProductCard/ProductCard";
 
 function Products() {
+  const [selectedCategory, setSelectedCategory] = useState("All");
+  const [searchTerm, setSearchTerm] = useState("");
+  const filteredProducts =
+  products.filter((product) => {
+
+    const matchesSearch =
+      product.name
+        .toLowerCase()
+        .includes(
+          searchTerm.toLowerCase()          
+        );
+
+    const matchesCategory =
+      selectedCategory === "All"
+        ? true
+        : product.category === selectedCategory;
+
+    return (
+      matchesSearch &&
+      matchesCategory
+    );
+  });
+
+  const categories = [
+  "All",
+  "Audio",
+  "Mobile Devices",
+  "Wearables",
+  "Computing",
+  "Smart Home",
+  "Accessories",
+  "Gaming",
+  "Entertainment"
+  ];
+
   return (
     <section
       style={{
@@ -10,10 +46,39 @@ function Products() {
     >
       <h1>All Products</h1>
 
+
+      <input
+        type="text"
+        placeholder="Search products..."
+        value={searchTerm}
+        onChange={(e) =>
+          setSearchTerm(e.target.value)
+        }
+      />
+
+      <div className="category-filters">
+
+        {categories.map((category) => (
+
+          <button
+            key={category}
+            onClick={() =>
+              setSelectedCategory(category)
+            }
+          >
+            {category}
+          </button>
+
+        ))}
+
+      </div>
+
       <div
         className="products-grid"
       >
-        {products.map((product) => (
+
+        
+        {filteredProducts.map((product) => (
           <ProductCard
             key={product.id}
             product={product}
